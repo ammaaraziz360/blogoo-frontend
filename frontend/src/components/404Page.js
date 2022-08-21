c2409adee91d1e3e688397918e93d1af54b534cc
Ammaar Mohammed <ammaaraziz360@gmail.com>
2022-08-21 01:51:00 -0500

    init

---
+++ frontend/src/components/404Page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const NotFoundPage = () => {
+    return (
+        <div className="container text-light text-center">
+            <h1>Oops! Error 404, Page not found</h1>
+        </div>
+    )
+        
+}
+
+export default NotFoundPage;
